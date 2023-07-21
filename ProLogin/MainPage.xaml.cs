d9aec23a4be510d9a5efec4ef762560b001960d9
Jose David Arce González <jarceg884@ulacit.ed.cr>
2023-07-20 23:03:50 -0600

    Semana 10

--- ProLogin/MainPage.xaml.cs
+++ ProLogin/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Net;
+using ProLogin;
+using System.Xml.Linq;
 
 namespace ProLogin
 {
@@ -16,31 +21,73 @@ namespace ProLogin
             loginBtn.Clicked += LoginBtn_Clicked;
         }
 
-        private void LoginBtn_Clicked(object sender, EventArgs e)
+
+
+        private async void LoginBtn_Clicked(object sender, EventArgs e)
         {
             // Obtener el usuario y la contraseña inssgresados por el usuario
-            string usuarioIngresado = usuario.Text;
-            string contraseñaIngresada = contraseña.Text;
 
-            // Datos de usuario y contraseña almacenados en variables
-            string usuarioGuardado = "admin";
-            string contraseñaGuardada = "admin";
+            HttpClient cliente = new HttpClient();
+            //este es el link para el el REST
 
-            // Verificar si el usuario y la contraseña son correctos
-            if (usuarioIngresado == usuarioGuardado && contraseñaIngresada == contraseñaGuardada)
+            string apiUrl = "https://g8a0c0fa384744a-avance2.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/usuarios/usuarios";
+            try
             {
-                // Iniciar sesión exitosamente
-                DisplayAlert("Inicio de sesión", "¡Inicio de sesión exitoso!", "Aceptar");
+                HttpResponseMessage response = await cliente.GetAsync(apiUrl);
 
-                //Cambio de página a menú en caso de ingreso éxitoso
-                ((NavigationPage)this.Parent).PushAsync(new Menu());
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string contenido = await response.Content.ReadAsStringAsync();
+
+                    ArrayProblem data = JsonConvert.DeserializeObject<ArrayProblem>(contenido);
+
+                    /*
+                     * Usuario: Wilson777
+                     * Contraseña: 1234
+                     * 
+                     * MariRex
+                     * 1234
+                     * 
+                     * SamiBeast
+                     * 1234
+                     * 
+                     * DavidFlo
+                     * 1234
+                    */
+
+                    bool entrada = true;
+                    foreach (var user in data.items)
+                    {
+                        if (user.username == usuario.Text && user.password == contraseña.Text)
+                        {
+                            // iniciar sesión exitosamente
+                            await DisplayAlert("Inicio de Sesión", "¡Inicio de Sesión Exitoso!", "Aceptar");
+
+                            //cambio de página a menú en caso de ingreso éxitoso
+                            await ((NavigationPage)this.Parent).PushAsync(new Menu());
+                            entrada = false;
+                        }
+                    }
+                    if (entrada)
+                    {
+                        await DisplayAlert("Alerta", "Credenciales Invalidos", "Ok");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"failed to retrieve data. status code: {response.StatusCode}");
+                }
 
             }
-            else
+
+            catch (Exception ex)
             {
-                // Mostrar mensaje de error si el usuario y/o la contraseña son incorrectos
-                DisplayAlert("Inicio de sesión", "Usuario y/o contraseña incorrectos", "Aceptar");
+                Console.WriteLine($"an error occurred: {ex.Message}");
             }
         }
+
+
     }
 }
---
+++ ProLogin/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Shapes;
+
+namespace ProLogin
+{
+    public class Usuario
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        //public Usuario(string username, string password) 
+        //{
+        //    this.username = username;
+        //    this.password = password;
+        //}
+    }
+
+    public class ArrayProblem
+    {
+        public Usuario[] items { get; set; }
+
+    }
+}
